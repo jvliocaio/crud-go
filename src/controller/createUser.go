bd60f987fbe652239b0c6d956620b104dd372f03
jvliocaio <juliocaionouv@gmail.com>
2025-06-10 10:43:31 -0300

    change int64 for int, adding createuser statements

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jvliocaio/crud-go/src/configuration/rest_err"
+	"github.com/jvliocaio/crud-go/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("moscou")
-	c.JSON(int(err.Code), err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
